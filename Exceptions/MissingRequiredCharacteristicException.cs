2356281a241b71fd1b5e5c34cc66b49c6b6e6b7f
Patryk Pastuszka <nov11157@gmail.com>
2025-05-19 17:15:51 +0200

    Warning / issues cleanup.

--- Exceptions/MissingRequiredCharacteristicException.cs
+++ Exceptions/MissingRequiredCharacteristicException.cs
@@ -2,7 +2,7 @@
 {
     /// <summary>
     /// Exception thrown when a required Bluetooth Low Energy characteristic is not found on a device.
-    /// This exception is typically thrown by the <see cref="BluetoothLowEnergyDeviceBase.Require"/> methods
+    /// This exception is typically thrown by the **Require** methods
     /// when attempting to access a characteristic that is essential for device operation.
     /// </summary>
     public sealed class MissingRequiredCharacteristicException()
