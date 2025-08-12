2997bf7da70aecddcb46203213e4ab7b60b7c4da
Patryk Pastuszka <nov11157@gmail.com>
2025-08-12 14:45:20 +0200

    Documentation

--- Data/BluetoothReconnectMode.cs
+++ Data/BluetoothReconnectMode.cs
@@ -1,10 +1,32 @@
 ﻿namespace IRIS.Bluetooth.Data
 {
+    /// <summary>
+    ///     Enum used to determine type of device reconnection methodology.
+    ///     Can be used to select device which is being reconnected after connection lost or
+    ///     disable reconnection whatsoever.
+    /// </summary>
     public enum BluetoothReconnectMode
     {
+        /// <summary>
+        ///     Device will be reconnected to same address as originally connected device
+        ///     a.k.a. device must be same one.
+        /// </summary>
         SameAddress,
+        
+        /// <summary>
+        ///     Device will be reconnected to any device that has same name as originally connected one.
+        /// </summary>
         SameName,
+        
+        /// <summary>
+        ///     Device will be automatically reconnected to any device that meets interface requirements.
+        ///     No other conditions are applied.
+        /// </summary>
         AnySimilarDevice,
+        
+        /// <summary>
+        ///     Device won't be reconnected when connection is lost.
+        /// </summary>
         Disabled
     }
 }
